3d29f722b316eb5050c485d0c002ad0a6db11ddf
Pig Monkey <pm@pig-monkey.com>
2012-04-03 10:31:05 -0700

    Attempt to import the WMD post form only if it exists.
    If the BLOG_WMD evaluates to True, the WMD Post Form should be imported.
    If it cannot be imported (because the BLOG_WMD setting is True, but the
    WMD app is not actually installed), fail silently.

--- vellum/admin.py
+++ vellum/admin.py
@@ -1,27 +1,37 @@
 from django.contrib import admin
 from vellum.models import *
-from vellum.forms import PostForm
 from vellum import settings
+
+try:
+    from vellum.forms import PostForm
+except ImportError:
+    pass
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
 admin.site.register(Category, CategoryAdmin)
 
 class PostAdmin(admin.ModelAdmin):
-    # If Django WMD is enabled, use the WMD-widgetized form and remove the
-    # markup field.
-    if settings.BLOG_WMD:
-        form = PostForm
-        display_fields = ('title', 'slug', 'author', 'body', 'tease',
-            'status', 'allow_comments', 'publish', 'categories', 'tags', ) 
-    else:
-        display_fields = ('title', 'slug', 'author', 'markup', 'body', 'tease',
-            'status', 'allow_comments', 'publish', 'categories', 'tags', ) 
-
+    display_fields = ('title', 'slug', 'author', 'markup', 'body', 'tease',
+                      'status', 'allow_comments', 'publish', 'categories',
+                      'tags',) 
     list_display  = ('title', 'publish', 'status', 'visits')
     list_filter   = ('publish', 'categories', 'status')
     search_fields = ('title', 'body')
     prepopulated_fields = {'slug': ('title',)}
+    
+    # If Django WMD is enabled, use the WMD-widgetized form.
+    if settings.BLOG_WMD:
+        try:
+            from vellum.forms import PostForm
+        except ImportError:
+            pass
+        else:
+            form = PostForm
+            # Remove the markup field from the display.
+            display_fields = ('title', 'slug', 'author', 'body', 'tease',
+                              'status', 'allow_comments', 'publish',
+                              'categories', 'tags',) 
 
     fieldsets = (
         (None, {
