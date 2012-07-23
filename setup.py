13500cb805c62823a378ce9155d30e39b037ab75
Pig Monkey <pm@pig-monkey.com>
2012-07-23 10:47:15 -0700

    Improve setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,26 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django-vellum',
+    packages = find_packages(),
     version = '0.1',
     description = 'A web log for Django.',
-    long_description = open('README.md').read(),
+    author = 'Peter Hogg',
+    author_email = 'peter@havenaut.net',
     url = 'https://github.com/pigmonkey/django-vellum',
-    author = 'Pig Monkey',
-    author_email = 'pm@pig-monkey.com',
-
-    packages = find_packages(),
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+    ],
+    long_description = open('README.md').read(),
+    include_package_data = True,
     zip_safe=False,
+    install_requires = ['django-inlineobjects',
+                        'django-simplesearch',
+                        'django-taggit',
+                        'django-taggit-templatetags',
+                        'django-markup',
+                        'Markdown',],
 )
