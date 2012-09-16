16e2fdc91af64b78915dfe4b4ebe8f9e83977836
Pig Monkey <pm@pig-monkey.com>
2012-09-15 18:39:37 -0700

    v1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='django-vellum',
     packages=find_packages(),
-    version='1.0',
+    version='1.1',
     description='A web log for Django.',
     author='Peter Hogg',
     author_email='peter@havenaut.net',
