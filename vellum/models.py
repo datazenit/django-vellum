a86dc887f416f1af0fe647e079998adfa0a30d7f
Lauris Dzilums <lauris.dzilums@gmail.com>
2017-06-03 20:59:45 +0300

    Fix model relations.

--- vellum/models.py
+++ vellum/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
 from django.db.models import permalink
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.template.defaultfilters import truncatewords_html
 from django.utils.timezone import now
 
@@ -13,6 +14,8 @@ from inlines.parser import inlines
 
 from vellum.managers import PublicManager
 from vellum import settings
+
+User = get_user_model()
 
 
 class Category(models.Model):
